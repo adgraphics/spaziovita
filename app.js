889b4aded45b5186953bdccdc671e441b8863f5a
Giulio Seregni <giulio.seregni@pingendo.com>
2017-04-10 17:09:10 +0200

    more

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 *********/
 
 
-var express = require('express');
+var express = require('express'); 
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
